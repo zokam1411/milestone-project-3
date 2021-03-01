eb287aa7ce9423fcf619db2e29dfd9cd83bd9f76
zokam1411 <zokam1411@gmail.com>
2021-03-01 10:11:46 +0000

    Display image in view_add

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
     $('select').formSelect();
     $('input#title, textarea#description').characterCounter();
+    $('.materialboxed').materialbox();
+    // Materialize selector validator
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
